8a72ae2b90964eb1fbad707834305b8a67cf0a09
UnixVextor <vitoon.sdf@gmail.com>
2023-04-07 19:03:50 +0700

    first commit

---
+++ BackEnd/BackEnd/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet("EmployeeList")]
+        public ActionResult List() {
+            return Ok();        
+        }
+    }
+}
