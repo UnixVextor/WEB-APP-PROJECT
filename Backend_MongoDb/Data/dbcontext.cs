db6c8907dcc15c6230d1605705a6c6380ff1e2c0
UnixVextor <vitoon.sdf@gmail.com>
2023-04-24 05:58:01 +0700

    Update:delete comment unnessary

--- Backend_MongoDb/Data/dbcontext.cs
+++ Backend_MongoDb/Data/dbcontext.cs
@@ -8,37 +8,13 @@ namespace Backend_MongoDb.Data {
                 
         }
 
-        // protected override void OnModelCreating(ModelBuilder modelBuilder){
-            // modelBuilder.Entity<Order>()
-            // .HasKey(x => x.Id);
-
-            // modelBuilder.Entity<Order>()
-            // .HasOne(x=> x.User)
-            // .WithMany(z => z.Orders)
-            // .HasForeignKey(x => x.UserId)
-            // .OnDelete(DeleteBehavior.ClientSetNull);
-
-            // modelBuilder.Entity<Order>()
-            // .HasOne(x=> x.Rider)
-            // .WithMany(z => z.Orders)
-            // .HasForeignKey(x => x.RiderId)
-            // .OnDelete(DeleteBehavior.ClientSetNull);
-
-            // modelBuilder.Entity<Order>()
-            //     .HasMany(e => e.Rider)
-            //     .WithMany(e => e.Orders);
-
-        // }
-
         // Add Table
         public DbSet<User> Users {get; set;}
-        // public DbSet<Rider>? Riders {get; set;}
         public DbSet<Restaurant>? Restaurants {get; set;}
         public DbSet<Order>? Oreders {get; set;}
         public DbSet<Menu>? Menus {get; set;}
         public DbSet<Menulist>? Menulists {get; set;}
 
-        // public DbSet<OrderRider> OrderRiders {get; set;}
         
     }
 }
